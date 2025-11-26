f55cfdd147242dd53a43040ca710797f30f61e2d
Ken Bao <47431779+ken-q-bao@users.noreply.github.com>
2025-11-26 14:01:45 -0500

    read into duckdb complete

--- readinto_duckdb.r
+++ readinto_duckdb.r
@@ -1,3 +1,10 @@
+# Purpose: Read in all .txt files from cdpr_data_unzip folder into cdpr_rawdata.duckdb
+# Date: June 12, 2024
+# Details: reads in .txt files mostly as-is, with all columns as varchar to avoid type issues.
+#          Uses duckdb's read_csv_auto function for faster reading and writing to duckdb.
+#          skips files that are already in the duckdb database to avoid duplication.
+#          If initial run, total run time is about 3 hrs.
+#          Total file size of cdpr_rawdata.duckdb is about 5.66 GB.
 library(duckdb)
 library(readr)
 
@@ -5,11 +12,43 @@ library(readr)
 files = list.files(path = "cdpr_data_unzip", pattern = ".txt", full.names = TRUE)
 files_basenames = gsub("\\.txt$", "", basename(files))
 
+#################### FASTER APPROACH ##########################
+# create list of queries to load each .txt file into duckdb
+queries = lapply(files, function(f) {
+  table_name = tools::file_path_sans_ext(basename(f))
+  qry = sprintf("CREATE TABLE %s AS SELECT * FROM read_csv_auto( 
+                                                      '%s', 
+                                                      header=TRUE, 
+                                                      all_varchar=TRUE, 
+                                                      strict_mode=FALSE,
+                                                      max_line_size = 8000000);", 
+                table_name, f)
+  return(qry)
+})
+
+con = dbConnect(duckdb::duckdb(), "cdpr_rawdata.duckdb")
+
+current_db_files = dbListTables(con)
+# only run queries for tables that do not already exist in the database
+## filter queries to only include those for tables that do not already exist in the database
+queries = queries[!files_basenames %in% current_db_files]
+
+lapply(queries, function(q) {
+  dbExecute(con, q)
+})
+dbDisconnect(con)
+
 #################### ALTERNATIVE SLOWER APPROACH ########################
 # # first read in the .txt files onto memory 1 at a time
 # # then write to duckdb
 # # utilize vectorization (lapply) to loop through files
-
+# 
+# test = read_delim(
+#   "cdpr_data_unzip/pur80.txt", 
+#   delim = "\t",
+#   col_names = TRUE
+# )
+# 
 # # define function to read in .txt file and write to duckdb
 # read_and_write_to_duckdb = function(file_path, con) {
 #   table_name = tools::file_path_sans_ext(basename(file_path))
@@ -33,24 +72,3 @@ files_basenames = gsub("\\.txt$", "", basename(files))
 # })
 # dbDisconnect(con)
 ##########################################################
-
-#################### FASTER APPROACH ##########################
-# create list of queries to load each .txt file into duckdb
-queries = lapply(files, function(f) {
-  table_name = tools::file_path_sans_ext(basename(f))
-  qry = sprintf("CREATE TABLE %s AS SELECT * FROM read_csv_auto('%s', header=TRUE, all_varchar=TRUE);", 
-                table_name, f)
-  return(qry)
-})
-
-con = dbConnect(duckdb::duckdb(), "cdpr_rawdata.duckdb")
-
-current_db_files = dbListTables(con)
-# only run queries for tables that do not already exist in the database
-## filter queries to only include those for tables that do not already exist in the database
-queries = queries[!files_basenames %in% current_db_files]
-
-lapply(queries, function(q) {
-  dbExecute(con, q)
-})
-dbDisconnect(con)

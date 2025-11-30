4f9b604b94e3a3099e759918b734d007cb8965b6
Ken Bao <47431779+ken-q-bao@users.noreply.github.com>
2025-11-30 18:30:25 -0500

    sandbox update

--- sandbox.r
+++ sandbox.r
@@ -17,6 +17,25 @@ comb_tbl = dbGetQuery(con, "SELECT *
                             FROM cdpr_combined
                             WHERE CAST(\"year\" AS INTEGER) BETWEEN 2020 AND 2021;")
 
+# Now run your interpolated query
+chem_val  <- "1601"
+start_yr  <- 2019   # keep these as integers
+end_yr    <- 2021
 
+sql <- sqlInterpolate(con, "
+  SELECT *
+  FROM cdpr_combined
+  WHERE (chem_code = ?chem_val OR chemical_code = ?chem_val)
+    AND CAST(\"year\" AS INTEGER) BETWEEN ?start_yr AND ?end_yr;
+", chem_val = chem_val, start_yr = start_yr, end_yr = end_yr)
 
+tbl <- dbGetQuery(con, sql)
+
+test_qry = "SELECT *
+            FROM cdpr_combined
+            WHERE CAST(\"year\" AS INTEGER) = 2021;"
+
+test = dbGetQuery(con, test_qry)
+
+test = dbGetQuery(con, "SELECT DISTINCT year FROM cdpr_combined;")
 dbDisconnect(con)
